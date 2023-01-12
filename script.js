a8bb8c8c358d098bb1bc468aa469617d16cba091
Utsav Singh <utsavgwa@gmail.com>
2023-01-13 03:24:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// unsplash api
+
+const count = 10;
+const apiKey = `UBrgUJBx7MwWckljBz_BeFPUUmc0sKKvMtvwLwV4IiM`;
+
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&orientation=landscape`;
+
+// get photos from unsplash api
