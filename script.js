df055cf7076fb81430fb95d72c76f53ca76d5416
Utsav Singh <utsavgwa@gmail.com>
2023-01-13 04:30:30 +0530

    created set attribute function, DRY code

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ const apiKey = `UBrgUJBx7MwWckljBz_BeFPUUmc0sKKvMtvwLwV4IiM`;
 
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&orientation=landscape`;
 
+// helper function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+  // here key is href, target, src, alt, title
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 // create elements for links and photos,n adding to DOM
 // for each item in this array we are going to run inside this every time, create an item, create an image,put image into item, put item in image container
 function displayPhotos() {
@@ -16,13 +24,22 @@ function displayPhotos() {
   photosArray.forEach((photo) => {
     // creating <a> to link to unsplash
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    // item.setAttribute("href", photo.links.html);
+    // item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
     // create <img> for photo
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // img.setAttribute("src", photo.urls.regular);
+    // img.setAttribute("alt", photo.alt_description);
+    // img.setAttribute("title", photo.alt_description);
     // put <img> inside <a> and then, both inside img-container element
     item.appendChild(img);
     imgContainer.appendChild(item);
