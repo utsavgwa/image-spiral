2f2596861cd7b70a9356166e4358e9aac9420a87
Utsav Singh <utsavgwa@gmail.com>
2023-01-13 03:51:37 +0530

    basic fetch request created

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ const apiKey = `UBrgUJBx7MwWckljBz_BeFPUUmc0sKKvMtvwLwV4IiM`;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&orientation=landscape`;
 
 // get photos from unsplash api
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // catch error here
+  }
+}
+
+// on load
+getPhotos();
